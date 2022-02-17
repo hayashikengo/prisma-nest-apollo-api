6bc8eace50f1de8ac5a220b9ef37c25d1a4cd299
hkengo <hayashikengojp@gmail.com>
2022-02-17 14:51:45 +0900

    $ npx prisma migrate dev --name user

---
+++ prisma/migrations/20220217054443_user/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(191) NOT NULL,
+    `displayName` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
